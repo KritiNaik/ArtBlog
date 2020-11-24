580d43f01694bd7dbbdf816744a39d53b51740c5
KritiNaik <naikkriti366@gmail.com>
2020-11-24 11:54:06 +0530

    Second commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
+
+
+$(document).ready(function(){
+    $(".post-btn").click(function(){
+       $(this).prev().toggle();
+       $(this).siblings('.dots').toggle();
+       if($(this).text()=='Read More'){
+     $(this).text('Read Less');
+       }
+       else{
+     $(this).text('Read More');
+       }
+    });
+ });
+$(document).ready(function () {
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    /** click event on toggle menu */
+    $toggleCollapse.click(function () {
+        $nav.toggleClass('collapse');
+    })
+
+    // owl-crousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
+    });
+
+
+    // click to scroll top
+    $('.move-up span').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
+
+    // AOS Instance
+    AOS.init();
+
+});
